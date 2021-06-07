b74ebb27c2548d26f0b4bf3b44ad543a525079a7
snehamore8 <snehamore8@gmail.com>
2021-06-07 15:30:54 +0530

    symbol changes ui

--- Calc.java
+++ Calc.java
@@ -5,7 +5,7 @@ public class Calc {
         System.out.println("Enter the first number"); int num1=s.nextInt();
         System.out.println("Enter the second number"); int num2=s.nextInt();
         int res;
-        System.out.println("Enter the operation from - +,-,/,*"); String op=s.next();
+        System.out.println("enter the operation from - +,-,/,*"); String op=s.next();
         switch(op){
             case "+": res=num1+num2;
             System.out.println("Addition of "+num1+" and "+num2+ " gives "+res);
